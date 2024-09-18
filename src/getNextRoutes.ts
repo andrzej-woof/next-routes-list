280702b40e2216421581ffd8f3b93e83f0ce2129
Andrzej Pasterczyk <andrzej.pasterczyk+github@gmail.com>
2024-09-18 17:07:29 +0200

    chore: refactor

--- src/getNextRoutes.ts
+++ src/getNextRoutes.ts
@@ -12,6 +12,15 @@ const getFilePaths = (dirPath: string, validExtensions?: string[], validName?: s
   );
 };
 
+const isPrivateRoute = (part: string) => part.startsWith("_");
+const isInterceptingRoute = (part: string) => part.startsWith("(") && !part.endsWith(")");
+const isParallelRoute = (part: string) => part.startsWith("@");
+
+const isGroupRoute = (part: string) => part.startsWith("(") && part.endsWith(")");
+
+const isAppIgnoredRoute = (part: string) => isPrivateRoute(part) || isParallelRoute(part) || isInterceptingRoute(part);
+
+
 export const getNextRoutes = (
 	src: string = ".",
 	extensions: string[] = ["tsx", "ts", "js", "jsx", "mdx"]
@@ -19,93 +28,49 @@ export const getNextRoutes = (
   const absoluteSrc = path.resolve(src);
   const dottedExtensions = extensions.map((ext) => ext.startsWith(".") ? ext : `.${ext}`);
 
-	// next app routes
-	const appPaths = getFilePaths(path.join(absoluteSrc, 'app'), dottedExtensions, 'page')
-
-	// next pages routes
+  const appRoot = path.join(absoluteSrc, 'app');
+	const appPaths = getFilePaths(appRoot, dottedExtensions, 'page')
 	const pagePaths = getFilePaths(path.join(absoluteSrc, 'pages'), dottedExtensions).filter((filePath) => !filePath.includes(`${path.sep}pages${path.sep}api${path.sep}`));
 
-	/**
-  appRoutes = [
-    '/app/(group)/blog/page.tsx', => route should be '/blog'
-    '/app/(group)/blog/[...slug]/page.tsx', => route should be '/blog/[...slug]'
-    '/app/@component/blog/page.tsx', // should remove, because it's not a page
-    '/app/blog/(..)list/page.tsx', // should remove, because it's not a page
-		'/app/_private/page.tsx', // should remove, because it's a private folder
-		'/app/%5Flog%5F/page.tsx', // should be '/_log_'
-  ]
-  */
-	const appRoutes = appPaths
-		.map((filePath) => {
-			const parts = filePath.split(absoluteSrc)[1]?.split(path.sep).filter(Boolean) ?? [];
-			const url: string[] = [];
+  const mapToRoutes = (paths: string[]): string[] => {
+    return paths
+      .map((filePath) => {
+        const isApp = filePath.startsWith(appRoot);
+        const parts = filePath.split(absoluteSrc)[1]?.split(path.sep).filter(Boolean) ?? [];
+        const urlParts: string[] = [];
 
-			for (let i = 0; i < parts.length; i++) {
-				let part = parts[i];
-				if (!part) continue;
+        for (let i = 1; i < parts.length; i++) {
+          let part = parts[i] as string;
 
-				if (i === 0 && part === "app") continue;
+          if (isApp && isAppIgnoredRoute(part)) {
+            return '';
+          }
 
-				const isPrivateRoute = part.startsWith("_");
-				if (isPrivateRoute) return null;
+          if (isApp && isGroupRoute(part)) {
+            continue;
+          }
 
-				const isGroupRoute = part.startsWith("(") && part.endsWith(")");
-				if (isGroupRoute) continue;
+          if (i === parts.length - 1) {
+            if (isApp) {
+              continue;
+            }
+            part = part.split(".").at(-2) ?? "";
+            if (part === "index") {
+              continue;
+            }
+          }
 
-				const isInterceptingRoute = part.startsWith("(") && !part.endsWith(")");
-				if (isInterceptingRoute) return null;
+          part = part.replace(/%5F/g, "_"); // Decode URL-encoded underscores
+          urlParts.push(part);
+        }
 
-				const isParallelRoute = part.startsWith("@");
-				if (isParallelRoute) return null;
+        return `/${urlParts.join("/")}`;
+      })
+      .filter(Boolean);
+  };
 
-				// ignore 'page.tsx' on url path
-				if (i === parts.length - 1) continue;
+  const appRoutes = mapToRoutes(appPaths);
+  const pagesRoutes = mapToRoutes(pagePaths);
 
-				// replace %5F to _
-				part = part.replace(/%5F/g, "_");
-
-				url.push(part);
-			}
-
-			return `/${url.join("/")}`;
-		})
-		.filter(Boolean);
-
-	/**
-  pageRoutes = [
-    '/pages/blog.js', => route should be '/blog'
-    '/pages/[slug].js', => route should be '/[...slug]'
-  ]
-  */
-	const pagesRoutes = pagePaths
-		.map((filePath) => {
-			const parts = filePath.split(absoluteSrc)[1]?.split(path.sep).filter(Boolean) ?? [];
-
-			const url: string[] = [];
-
-			for (let i = 0; i < parts.length; i++) {
-				let part = parts[i];
-				if (!part) continue;
-
-				if (i === 0 && part === "pages") continue;
-
-				if (i === parts.length - 1) {
-					part = part.split(".").at(-2) ?? "";
-
-					if (part === "index") {
-						continue;
-					}
-				}
-
-				url.push(part);
-			}
-
-			return `/${url.join("/")}`;
-		})
-		.filter(Boolean);
-
-	const unDuplicatedRoutes = Array.from(
-		new Set([...appRoutes, ...pagesRoutes])
-	);
-	return unDuplicatedRoutes;
-}
+  return Array.from(new Set([...appRoutes, ...pagesRoutes]));
+};
